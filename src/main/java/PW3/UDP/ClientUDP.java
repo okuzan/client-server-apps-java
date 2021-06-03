10c848fed23cb41548fd20725cad0eca01a31094
okuzan <49767049+okuzan@users.noreply.github.com>
2021-06-04 01:05:30 +0300

    Update ClientUDP.java

--- src/main/java/PW3/UDP/ClientUDP.java
+++ src/main/java/PW3/UDP/ClientUDP.java
@@ -1,60 +1,89 @@
 package PW3.UDP;
 
 import HW2.Packet.Packet;
+import HW2.Packet.PacketRandomizer;
 
-import java.net.SocketAddress;
+import java.io.IOException;
+import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class ClientProcessor extends Thread {
-    private final Queue<Packet> queue = new ConcurrentLinkedQueue<>();
-    private final int userID;
-    private long lastPackedID = 0;
-    private long msgCounter = 0;
-    private SocketAddress socketAddress;
+import static HW2.Packet.Packet.decodePackage;
 
-    public ClientProcessor(final int userID) {
-        super("Client processor" + userID);
-        this.userID = userID;
-        start();
+public class ClientUDP {
+    private final DatagramSocket socket;
+    private final Queue<Packet> QUEUE = new ConcurrentLinkedQueue<>(); // to resend if sth goes wrong
+    private final int clientID;
+    private int msgCounter = 0;
+    private long lastPacketId = 0;
+
+    public ClientUDP(int clientID) throws SocketException {
+        this.clientID = clientID;
+        this.socket = new DatagramSocket();
+        Packet packet = PacketRandomizer.generate();
+        try {
+            send(Packet.encodePackage(packet));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void acceptPacket(Packet packet, SocketAddress socketAddress) {
-        System.out.println("ACCEPTED: " + packet);
-        this.socketAddress = socketAddress;
-        queue.add(packet);
+    public void send(final byte[] bytes) throws UnknownHostException {
+        Packet packet = new Packet((byte) 10, ++msgCounter, 10, clientID, bytes);
+        QUEUE.add(packet);
+        byte[] encoded = Packet.encodePackage(packet);
+        DatagramPacket datagramPacket = new DatagramPacket(encoded, encoded.length, InetAddress.getByName(null), ServerUDP.PORT);
+        try {
+            socket.send(datagramPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public void run() {
-        while (true) {
-            Packet packet = queue.poll();
-            if (packet != null) {
-                System.out.format("[client %s] Processing packet %s\n", userID, packet.getPacketId());
-                Packet responsePacket;
-                //checking if no losses
-                System.out.println("THIS PACKET ID " + packet.getPacketId());
-                System.out.println("LAST PACKET ID " + lastPackedID);
-                if (packet.getPacketId() != lastPackedID + 1) {
-                    responsePacket = new Packet((byte) 0, ++msgCounter, 911, 0, (String.valueOf(lastPackedID)).getBytes(StandardCharsets.UTF_8));
-                    System.out.println("RESEND REQUESTED");
-                    ServerQueue.QUEUE.add(new AddressedPacket(responsePacket, socketAddress));
-                } else {
-                    responsePacket = new Packet((byte) 0, ++msgCounter, 200, 0, String.valueOf(packet.getPacketId()).getBytes(StandardCharsets.UTF_8));
-                    lastPackedID = packet.getPacketId();
-                    System.out.println("SUCCESS REPORTED");
-                    ServerQueue.QUEUE.add(new AddressedPacket(responsePacket, socketAddress));
-
-                    //resending if needed
-                    if (packet.getCode() == 911) for (int i = 0; i < queue.size(); i++)
-                        for (Packet packet1 : queue)
-                            if (packet1.getPacketId() == Long.parseLong(new String(packet.getMsg())) + 1) {
-                                ServerQueue.QUEUE.add(new AddressedPacket(packet1, socketAddress));
-                                System.out.println("SERVER ADDED LOST MSG TO ITS QUEUE");
-                            }
+    //resending lost packets
+    private void process(Packet packet) throws UnknownHostException {
+        long pId = packet.getPacketId();
+        System.out.println("LAST PACKET ID " + lastPacketId);
+        System.out.println("SERVER PACKET ID " + pId);
+        if (pId > lastPacketId + 1) { //if less it's resending
+            Packet responsePacket = new Packet((byte) 0, msgCounter++, 911, 0, (String.valueOf(lastPacketId)).getBytes(StandardCharsets.UTF_8));
+            send(Packet.encodePackage(responsePacket));
+            System.out.println("RESEND REQUESTED BY CLIENT");
+        }
+        else {
+            lastPacketId = pId;
+            if (packet.getCode() == 200) {
+                System.out.println("SERVER RECEIVED PACKET, DELETING FROM RESERVE QUEUE...");
+                QUEUE.removeIf(p -> p.getPacketId() == pId);
+            }
+            if (packet.getCode() == 911) { //means resend
+                System.out.println("SERVER REQUESTED RESENDING");
+                long packetIdToResend = Long.parseLong(new String(packet.getMsg()));
+                for (int i = 0; i < QUEUE.size(); i++) {
+                    for (Packet packet1 : QUEUE) {
+                        if (packet1.getPacketId() == packetIdToResend + 1)
+                            send(Packet.encodePackage(packet));
+                    }
                 }
             }
+
         }
+    }
+
+    public Packet receive() throws UnknownHostException {
+        DatagramPacket datagramPacket = new DatagramPacket(new byte[1000], 1000);
+        Packet packet = null;
+        try {
+            socket.receive(datagramPacket);
+            packet = decodePackage(Arrays.copyOfRange(datagramPacket.getData(), 0, datagramPacket.getLength()));
+            process(packet);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return packet;
     }
 }
